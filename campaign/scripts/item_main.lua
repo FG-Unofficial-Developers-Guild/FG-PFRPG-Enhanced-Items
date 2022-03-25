268f8b8f7e00217c23c3e6beb6b15cd81a03cb6f
Wil Thieme <wil.thieme@protonmail.com>
2022-03-24 21:06:29 -0400

    better line length and formatting

--- campaign/scripts/item_main.lua
+++ campaign/scripts/item_main.lua
@@ -1,28 +1,17 @@
 -- 
 -- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
+function onInit() update(); end
 
-function onInit()
-	update();
-end
+function VisDataCleared() update(); end
 
-function VisDataCleared()
-	update();
-end
-
-function InvisDataAdded()
-	update();
-end
+function InvisDataAdded() update(); end
 
 function updateControl(sControl, bReadOnly, bID)
-	if not self[sControl] then
-		return false;
-	end
-		
-	if not bID then
-		return self[sControl].update(bReadOnly, true);
-	end
-	
+	if not self[sControl] then return false; end
+
+	if not bID then return self[sControl].update(bReadOnly, true); end
+
 	return self[sControl].update(bReadOnly);
 end
 
@@ -31,24 +20,12 @@ function getItemType()
 	local sType = string.lower(type.getValue());
 	local sSubtype = string.lower(subtype.getValue());
 
-	if sType:match('weapon') then
-		bWeapon = true;
-	end
-	if sType:match('armor') then
-		bArmor = true;
-	end
-	if sType:match('wand') then
-		bWand = true;
-	end
-	if sType:match('staff') then
-		bStaff = true;
-	end
-	if sType:match('wondrous item') then
-		bWondrous = true;
-	end
-	if sType:match('shield') or sSubtype:match('shield') then
-		bShield = true;
-	end
+	if sType:match('weapon') then bWeapon = true; end
+	if sType:match('armor') then bArmor = true; end
+	if sType:match('wand') then bWand = true; end
+	if sType:match('staff') then bStaff = true; end
+	if sType:match('wondrous item') then bWondrous = true; end
+	if sType:match('shield') or sSubtype:match('shield') then bShield = true; end
 
 	return bWeapon, bArmor, bShield, bWand, bStaff, bWondrous;
 end
@@ -62,39 +39,39 @@ function update()
 
 	local bSection1 = false;
 	if bOptionID and Session.IsHost then
-		if updateControl("nonid_name", bReadOnly, true) then bSection1 = true; end;
+		if updateControl('nonid_name', bReadOnly, true) then bSection1 = true; end
 	else
-		updateControl("nonid_name", false);
+		updateControl('nonid_name', false);
 	end
 	if bOptionID and (Session.IsHost or not bID) then
-		if updateControl("nonidentified", bReadOnly, true) then bSection1 = true; end;
+		if updateControl('nonidentified', bReadOnly, true) then bSection1 = true; end
 	else
-		updateControl("nonidentified", false);
+		updateControl('nonidentified', false);
 	end
 
 	local bSection2 = false;
-	if updateControl("type", bReadOnly, bID) then bSection2 = true; end
-	if updateControl("subtype", bReadOnly, bID) then bSection2 = true; end
-	
-	local bSection3 = false;
-	if updateControl("cost", bReadOnly, bID) then bSection3 = true; end
-	if updateControl("weight", bReadOnly, bID) then bSection3 = true; end
-	if updateControl("size", bReadOnly, bID) then bSection3 = true; end
-	
-	local bSection4 = false;
-	if updateControl("damage", bReadOnly, bID and bWeapon) then bSection4 = true; end
-	if updateControl("damagetype", bReadOnly, bID and bWeapon) then bSection4 = true; end
-	if updateControl("critical", bReadOnly, bID and bWeapon) then bSection4 = true; end
-	if updateControl("range", bReadOnly, bID and bWeapon) then bSection4 = true; end
-	
-	if updateControl("ac", bReadOnly, bID and bArmor) then bSection4 = true; end
-	if updateControl("maxstatbonus", bReadOnly, bID and bArmor) then bSection4 = true; end
-	if updateControl("checkpenalty", bReadOnly, bID and bArmor) then bSection4 = true; end
-	if updateControl("spellfailure", bReadOnly, bID and bArmor) then bSection4 = true; end
-	if updateControl("speed30", bReadOnly, bID and bArmor) then bSection4 = true; end
-	if updateControl("speed20", bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('type', bReadOnly, bID) then bSection2 = true; end
+	if updateControl('subtype', bReadOnly, bID) then bSection2 = true; end
 
-	if updateControl("charge", bReadOnly, bID and (bWand or bStaff)) then
+	local bSection3 = false;
+	if updateControl('cost', bReadOnly, bID) then bSection3 = true; end
+	if updateControl('weight', bReadOnly, bID) then bSection3 = true; end
+	if updateControl('size', bReadOnly, bID) then bSection3 = true; end
+
+	local bSection4 = false;
+	if updateControl('damage', bReadOnly, bID and bWeapon) then bSection4 = true; end
+	if updateControl('damagetype', bReadOnly, bID and bWeapon) then bSection4 = true; end
+	if updateControl('critical', bReadOnly, bID and bWeapon) then bSection4 = true; end
+	if updateControl('range', bReadOnly, bID and bWeapon) then bSection4 = true; end
+
+	if updateControl('ac', bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('maxstatbonus', bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('checkpenalty', bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('spellfailure', bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('speed30', bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('speed20', bReadOnly, bID and bArmor) then bSection4 = true; end
+
+	if updateControl('charge', bReadOnly, bID and (bWand or bStaff)) then
 		bSection4 = true;
 		maxcharges.setReadOnly(bReadOnly);
 		charge.setReadOnly(false);
@@ -107,33 +84,29 @@ function update()
 		maxcharges_label.setVisible(false);
 	end
 
-	if updateControl("equipslot", bReadOnly, bID and bWondrous) then bSection4 = true; end
+	if updateControl('equipslot', bReadOnly, bID and bWondrous) then bSection4 = true; end
 
-	if updateControl("properties", bReadOnly, bID and (bWeapon or bArmor)) then bSection4 = true; end
-	
+	if updateControl('properties', bReadOnly, bID and (bWeapon or bArmor)) then bSection4 = true; end
+
 	local bSection5 = false;
-	if updateControl("bonus", bReadOnly, bID and (bWeapon or bArmor)) then bSection5 = true; end
-	if updateControl("aura", bReadOnly, bID) then bSection5 = true; end
-	if updateControl("cl", bReadOnly, bID) then bSection5 = true; end
-	if updateControl("prerequisites", bReadOnly, bID) then bSection5 = true; end
-	
+	if updateControl('bonus', bReadOnly, bID and (bWeapon or bArmor)) then bSection5 = true; end
+	if updateControl('aura', bReadOnly, bID) then bSection5 = true; end
+	if updateControl('cl', bReadOnly, bID) then bSection5 = true; end
+	if updateControl('prerequisites', bReadOnly, bID) then bSection5 = true; end
+
 	local bSection6 = bID;
 	description.setVisible(bID);
 	description.setReadOnly(bReadOnly);
-	updateControl("sourcebook", bReadOnly, bID);
-	
+	updateControl('sourcebook', bReadOnly, bID);
+
 	local bSection7 = false;
 	divider6.setVisible(false);
 	gmonly_label.setVisible(false);
 	gmonly.setVisible(false);
-	
-	if Session.IsHost then
-		if updateControl("gmonly", bReadOnly, bOptionID) then bSection7 = true; end
-	end
-	if Session.IsHost then 
-		divider6.setVisible((bSection1 or bSection2 or bSection3 or bSection4 or bSection5) and bSection7);
-	end
-	
+
+	if Session.IsHost then if updateControl('gmonly', bReadOnly, bOptionID) then bSection7 = true; end end
+	if Session.IsHost then divider6.setVisible((bSection1 or bSection2 or bSection3 or bSection4 or bSection5) and bSection7); end
+
 	divider.setVisible(bSection1 and bSection2);
 	divider2.setVisible((bSection1 or bSection2) and bSection3);
 	divider3.setVisible((bSection1 or bSection2 or bSection3) and bSection4);
