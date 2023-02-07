74c149cd59dc03034a8302eefa3645e60f22ee9e
Wil Thieme <wil.thieme@protonmail.com>
2023-02-07 11:32:51 -0500

    change charge counter back to number

--- campaign/scripts/item_main_EI.lua
+++ campaign/scripts/item_main_EI.lua
@@ -1,6 +1,7 @@
 --
 -- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
+-- luacheck: globals subtype
 
 local function getItemTypes()
 	local tTypes = {
